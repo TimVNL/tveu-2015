c100d040232f291070f4bcdf21443a176f6e57a4
Tim Veluwenkamp <timveluwenkamp@noxqs.nl>
2015-06-16 21:47:13 +0200

    old site working with scss and fixed mailer, next fallback.js

---
+++ index.php
@@ -0,0 +1,343 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+
+<head>
+ <meta charset="utf-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <meta name="description" content="">
+ <meta name="author" content="">
+ <title>Landing Page - Start Bootstrap Theme</title>
+ <link href="css/main.css" rel="stylesheet">
+ <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"</script>
+
+ </script>
+</head>
+
+<body data-spy="scroll" data-target=".navbar" data-offset="0">
+ <header id="start">
+ 			<div id="banner" class="row">
+ 				<ul class="backgrounds list-unstyled">
+ 					<li><span>Image 1</span></li>
+ 				</ul>
+ 			</div>
+ 			<div class="jumbotron">
+ 				<div class="container">
+ 					<img src="" alt="" class="img-circle">
+ 					<h1>I'm Tim Veluwenkamp</h1>
+ 					<h3>I Design &amp; Build Beautiful Crafted Web Experiences</h3>
+ 				</div>
+ 			</div>
+
+  <nav class="navbar navbar-inverse" role="navigation">
+   <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"> </span>
+      <span class="icon-bar"> </span>
+      <span class="icon-bar"> </span>
+     </button>
+     <a class="navbar-brand hidden-xs hidden-sm" href="#">Tim Veluwenkamp</a>
+     <a class="navbar-brand hidden-md hidden-lg" href="#">Tim V.</a>
+    </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="navbar-collapse">
+     <ul class="nav navbar-nav">
+      <li class="active"><a href="#start">Home</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#skills">Skills</a></li>
+      <li><a href="#experience">Experience</a></li>
+      <li><a href="#education">Education</a></li>
+      <li><a href="#portfolio">Portfolio</a></li>
+      <li><a href="#contact">Contact</a></li>
+    </div><!-- /.navbar-collapse -->
+   </div>
+  </nav>
+  <p class="bg-info text-center" style="padding: 10px 0; margin-bottom: 0px;">My website is still under active development...</p>
+ </header>
+ <!--[if lt IE 8]>
+ <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+ <![endif]-->
+
+ <section id="about">
+  <div class="container">
+   <div class="page-header">
+        <h2>About Me <small>A small introduction about myself</small></h2>
+       </div>
+     <div class="row">
+       <div class=" col-xs-12 col-sm-12 col-md-8">
+         <h4>Tim Veluwenkamp<br/>
+         <small>Web Developer from Arnhem</small></h4>
+         <p>I'm obsessed with making things and even more obsessed with making things better. I created me fist website at the age of 11 with the help of Microsoft Frontpage. After graduating school I got in touch with Noxqs a Software Company with some unique and special people, I have bieng working here since October 2010 now and I still love my work! </p>
+         <p>But I do more than just creating beautiful, clean and scemitic website I also love to advice people how thet can use apps and computers to improve the quality of life. I think it's everybody's right to be online no mather what you have or what you do. </p>
+      </div>
+      <div class="hidden-xs hidden-sm col-md-4">
+        <img src="" alt="" style="width:360px; margin-top: 20px;" />
+       </div>
+     </div>
+    </div>
+ </section>
+
+ <section id="skills">
+  <div class="container">
+   <div class="page-header">
+    <h2>Skills <small>I can say i'm quite good at</small></h2>
+   </div>
+     <div class="row">
+         <div class="col-xs-6 col-md-3 skill"> <span class="chart" data-percent="90"> <span class="percent"></span> </span>
+           <h4>HTML5 / CSS3</h4>
+         </div>
+         <div class="col-xs-6 col-md-3 skill"> <span class="chart" data-percent="80"> <span class="percent"></span> </span>
+           <h4>WORDPRESS</h4>
+         </div>
+          <div class="col-xs-6 col-md-3 skill"> <span class="chart" data-percent="80"> <span class="percent"></span> </span>
+           <h4>WINDOWS XP, 7, 8 &amp; SERVER 2008</h4>
+         </div>
+         <div class=" col-xs-6 col-md-3 skill"> <span class="chart" data-percent="75"> <span class="percent"></span> </span>
+           <h4>ABBYY FINEREADER OCR &amp; KURTWEIL 3000</h4>
+         </div>
+         <div class="col-xs-6 col-md-3 skill"> <span class="chart" data-percent="70"> <span class="percent"></span> </span>
+           <h4>MOBILE DEVELOPMENT (ANDROID &amp; IOS)</h4>
+         </div>
+         <div class="col-xs-6 col-md-3 skill"> <span class="chart" data-percent="70"> <span class="percent"></span> </span>
+           <h4>PHOTOSHOP</h4>
+         </div>
+          <div class="col-xs-6 col-md-3 skill"> <span class="chart" data-percent="70"> <span class="percent"></span> </span>
+           <h4>PHP / MYSQL</h4>
+         </div>
+         <div class="col-xs-6 col-md-3 skill"> <span class="chart" data-percent="60"> <span class="percent"></span> </span>
+           <h4>LINUX</h4>
+         </div>
+         <div class="col-xs-6 col-md-3 skill"> <span class="chart" data-percent="60"> <span class="percent"></span> </span>
+           <h4>GIT</h4>
+         </div>
+         <div class="col-xs-6 col-md-3 skill"> <span class="chart" data-percent="50"> <span class="percent"></span> </span>
+           <h4>JQUERY</h4>
+         </div>
+         <div class="col-xs-6 col-md-3 skill"> <span class="chart" data-percent="40"> <span class="percent"></span> </span>
+           <h4>Ruby On Rails</h4>
+         </div>
+       </div>
+     </div>
+ </section>
+
+ <section id="experience">
+  <div class="container">
+   <div class="page-header">
+        <h2>Work Experience <small>My previous associations</small></h2>
+       </div>
+      <div class="row">
+        <div class="col-xs-4 col-md-2">
+          <div class="years">Oct, 2010
+           <br/>Present</div>
+       </div>
+       <div class="col-xs-8 col-md-10 details">
+        <h4>Webdeveloper<br/>
+        <small><i class="fa fa-map-marker"> </i><a href="https://plus.google.com/105807787564855848545/about"> NoXqs B.V.</a></small></h4>
+         <p>The work of NoXqs relate to the development and maintenance of new and existing products. In addition, NoXqs is working for small and large clients in web design and application development,</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-4 col-md-2">
+          <div class="years">2008
+           <br/>Sept, 2010
+          </div>
+       </div>
+       <div class="col-xs-8 col-md-10 details">
+        <h4>ICT Coordinator Assistant<br/>
+        <small><i class="fa fa-map-marker"> </i><a href="http://prisma-arnhem.nl"> Het Prisma, Arnhem</a></small></h4>
+         <p>The work of NoXqs relate to the development and maintenance of new and existing products. In addition, NoXqs is working for small and large clients in web design and application development,</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-4 col-md-2">
+          <div class="years">2008
+           <br/>Sept, 2010
+          </div>
+       </div>
+       <div class="col-xs-8 col-md-10 details">
+        <h4>Employee Computer Advies Team<br/>
+        <small><i class="fa fa-map-marker"> </i><a href="http://www.deonderwijsspecialisten.nl"> De Onderwijsspecialisten, Arnhem </a></small></h4>
+         <p>The work of NoXqs relate to the development and maintenance of new and existing products. In addition, NoXqs is working for small and large clients in web design and application development,</p>
+        </div>
+      </div>
+    </div>
+ </section>
+
+ <section id="education">
+  <div class="container">
+   <div class="page-header">
+        <h2>Education &amp; Diploma's <small>My (boring) education carreer </small></h2>
+       </div>
+      <div class="row">
+       <div class="col-xs-8 col-md-10 details">
+        <h4>Practical Sector Orientation (VMBO)<br/>
+        <small><i class="fa fa-map-marker"> </i> <a href="http://www.mariendael.nl/">MariÃ«ndael VSO, Arnhem</a></small></h4>
+         <p>SG MariÃ«ndael VSO provides secondary special education for students with physical disabilities and chronically ill students aged 12 to 20 years.  The school guides students to be independently as possible and integrated into society.</p>
+        </div>
+    <div class="col-xs-4 col-md-2">
+          <div class="years">2004
+           <br/>2009</div>
+       </div>
+       <div class="col-xs-8 col-md-10 details">
+        <h4>Basic Education<br/>
+        <small><i class="fa fa-map-marker"> </i> <a href="http://www.ssonml.nl/ulingshof/">Mytylschool Ulingshof, Venlo</a></small></h4>
+         <p>Mytylschool Ulingshof special school is a school where education and physiotherapy are integrated.
+The program is tailored to individual development opportunities.</p>
+        </div>
+    <div class="col-xs-4 col-md-2">
+          <div class="years">2000
+           <br/>2004</div>
+       </div>
+      </div>
+    </div><!-- /.container -->
+ </section>
+
+ <section id="portfolio">
+  <div class="container">
+   <div class="page-header">
+        <h2>Portfolio <small>Masterpieces I created</small></h2>
+       </div>
+       <!-- Nav tabs -->
+       <ul class="nav nav-tabs">
+         <li class="active"><a href="#websites" data-toggle="tab">Websites</a></li>
+         <li><a href="#logos" data-toggle="tab">Logo's</a></li>
+       </ul>
+
+       <!-- Tab panes -->
+       <div class="tab-content">
+         <div class="tab-pane active" id="websites">
+           <div class="row">
+             <div class="col-xs-6 col-md-3">
+               <a href="http://noxqs.nl" class="thumbnail">
+                 <img src="img/portfolio/noxqs-noxqs2014.png" alt="">
+               </a>
+             </div><!--.col-->
+
+             <div class="col-xs-6 col-md-3">
+               <a href="http://droomvillalux.nl" class="thumbnail">
+                 <img src="img/portfolio/noxqs-droomvillalux.png" alt="">
+               </a>
+             </div><!--.col-->
+
+             <div class="col-xs-6 col-md-3">
+               <a href="http://stefka.nu" class="thumbnail">
+                 <img src="img/portfolio/noxqs-stefka.png" alt="">
+               </a>
+             </div><!--.col-->
+
+             <div class="col-xs-6 col-md-3">
+               <a href="http://flitzenzo.nl" class="thumbnail">
+                 <img src="img/portfolio/noxqs-flitzenzo.png" alt="">
+               </a>
+             </div><!--.col-->
+
+             <div class="col-xs-6 col-md-3">
+               <a href="http://codenamelungo.net" class="thumbnail">
+                 <img src="img/portfolio/priv-codenamelungo.png" alt="">
+               </a>
+             </div><!--.col-->
+
+             <div class="col-xs-6 col-md-3">
+               <a href="http://roc-gorinchem.nl" class="thumbnail">
+                 <img src="img/portfolio/noxqs-roc.png" alt="">
+               </a>
+             </div><!--.col-->
+
+             <div class="col-xs-6 col-md-3">
+               <a href="http://timzorg.nl" class="thumbnail">
+                 <img src="img/portfolio/noxqs-timzorg.png" alt="">
+               </a>
+             </div><!--.col-->
+
+             <div class="col-xs-6 col-md-3">
+               <a href="http://www.bakkerlegal.com" class="thumbnail">
+                 <img src="img/portfolio/noxqs-bakkerlegal.png" alt="">
+               </a>
+             </div><!--.col-->
+
+             <div class="col-xs-6 col-md-3">
+               <a href="http://inevermee.com" class="thumbnail">
+                 <img src="img/portfolio/noxqs-inevermee.png" alt="">
+               </a>
+             </div><!--.col-->
+
+             <div class="col-xs-6 col-md-3">
+               <a href="http://apple4ever.nl" class="thumbnail">
+                 <img src="img/portfolio/noxqs-apple4ever.png" alt="">
+               </a>
+             </div><!--.col-->
+
+           </div><!--.row-->
+         </div><!--.tab-pane -->
+         <div class="tab-pane" id="logos">...</div>
+       </div>
+  </div>
+ </section>
+
+ <section id="contact">
+  <div class="container">
+   <div class="page-header">
+        <h2>Contact <small>Connect with me!</small></h2>
+       </div>
+   <div class="row">
+    <div class="col-xs-12 col-md-8">
+     <form name="contactform" method="post" action="mailer.php" class="form-horizontal" role="form">
+      <div class="form-group">
+       <label for="inputName" class="col-lg-2 control-label">Name</label>
+       <div class="col-lg-10">
+        <input type="text" class="form-control" id="inputName" name="inputName" placeholder="Your Name">
+       </div>
+      </div>
+      <div class="form-group">
+       <label for="inputEmail1" class="col-lg-2 control-label">Email</label>
+       <div class="col-lg-10">
+        <input type="text" class="form-control" id="inputEmail" name="inputEmail" placeholder="Your Email">
+       </div>
+      </div>
+      <div class="form-group">
+       <label for="inputSubject" class="col-lg-2 control-label">Subject</label>
+       <div class="col-lg-10">
+        <input type="text" class="form-control" id="inputSubject" name="inputSubject" placeholder="Subject Message">
+       </div>
+      </div>
+      <div class="form-group">
+       <label for="inputPassword1" class="col-lg-2 control-label">Message</label>
+       <div class="col-lg-10">
+        <textarea class="form-control" rows="4" id="inputMessage" name="inputMessage" placeholder="Your message..."></textarea>
+       </div>
+      </div>
+      <div class="form-group">
+       <div class="col-lg-offset-2 col-lg-10">
+        <button type="submit" class="btn btn-primary">
+         Send Message
+        </button>
+       </div>
+      </div>
+     </form>
+    </div>
+    <div class="col-xs-12 col-md-4">
+     <div class="col-xs-6 col-md-6">
+      <p><a type="button" class="btn btn-lg btn-block btn-default" href="https://bitbucket.org/TimVNL" target="_blank"><i class="fa fa-bitbucket"></i> Bitbucket</a></p>
+      <p><a type="button" class="btn btn-lg btn-block btn-default" href="https://www.linkedin.com/in/timvnl" target="_blank"><i class="fa fa-linkedin"></i> LinkedIN</a></p>
+      <p><a type="button" class="btn btn-lg btn-block btn-default" href="https://twitter.com/TimV_"><i class="fa fa-twitter"></i> Twitter</a></p>
+     </div>
+     <div class="col-xs-6 col-md-6">
+      <p><a type="button" class="btn btn-lg btn-block btn-default"><i class="fa fa-facebook" href="https://www.facebook.com/TimVNL" target="_blank"></i> Facebook</a></p>
+      <p><a type="button" class="btn btn-lg btn-block btn-default" href="https://github.com/TimVNL" target="_blank"><i class="fa fa-github-alt"></i> Github</a></p>
+     </div>
+    </div>
+   </div>
+  </div>
+ </section>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/easy-pie-chart/2.1.4/jquery.easypiechart.min.js"></script>
+  <script src="js/main.js"></script>
+
+
+ </body>
+
+</html>
