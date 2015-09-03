050473462020ffc7f4ff471d715a490f3b3969a7
Tim Veluwenkamp <timveluwenkamp@noxqs.nl>
2015-09-03 15:30:12 +0200

    new timeline design for work, education etc.. + new portfolio tabs

--- index.php
+++ index.php
@@ -33,8 +33,10 @@
           <li><a href="#education">Education</a></li>
           <li><a href="#portfolio">Portfolio</a></li>
           <li><a href="#contact">Contact</a></li>
-      </div><!-- /.navbar-collapse -->
-    </div><!--/.container -->
+      </div>
+      <!-- /.navbar-collapse -->
+    </div>
+    <!--/.container -->
   </nav>
 
   <div id="banner" class="banner">
@@ -46,11 +48,16 @@
           <div class="col-md-8 col-md-offset-2">
             <h1 class="text-center">I'm Tim Veluwenkamp</h1>
             <p class="lead text-center">I Design &amp; Build Beautiful Crafted Web Experiences</p>
-          </div><!--/.col-md-8 col-md-offset-2 -->
-        </div><!--/.row -->
-      </div><!--/.container -->
-    </div><!--/.banner-caption -->
-  </div><!--/#banner -->
+          </div>
+          <!--/.col-md-8 col-md-offset-2 -->
+        </div>
+        <!--/.row -->
+      </div>
+      <!--/.container -->
+    </div>
+    <!--/.banner-caption -->
+  </div>
+  <!--/#banner -->
   <!--[if lt IE 8]>
  <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
  <![endif]-->
@@ -64,16 +71,23 @@
       </div>
       <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-8">
-          <h4>Tim Veluwenkamp<br/><small>Web Developer from Arnhem</small></h4>
+          <h4>Tim Veluwenkamp
+            <br/>
+            <small>Web Developer from Arnhem</small>
+          </h4>
           <p>I'm obsessed with making things and even more obsessed with making things better. I created me first website at the age of 11 with the help of Microsoft Frontpage. After graduating school I came in touch with NoXqs a Software Company with some
             unique and special people, I have been working here since October 2010 now and I still love my work! </p>
           <p>But I do more than just creating beautiful, clean websites I also love to advice people how that can use apps and computers to improve the quality of life. I think it's everybody's right to be online no mather what you have or what you do. </p>
-        </div><!--/.col-xs-12 col-sm-12 col-md-8 -->
+        </div>
+        <!--/.col-xs-12 col-sm-12 col-md-8 -->
         <div class="hidden-xs hidden-sm col-md-4">
           <img class="img-circle" src="img/me.png" alt="" style="width: 232px; margin-top: -59px;" />
-        </div><!--/.hidden-xs hidden-sm col-md-4 -->
-      </div><!--/.row -->
-    </div><!--.container -->
+        </div>
+        <!--/.hidden-xs hidden-sm col-md-4 -->
+      </div>
+      <!--/.row -->
+    </div>
+    <!--.container -->
   </section>
 
   <section id="skills">
@@ -84,76 +98,65 @@
       <div class="row">
         <div class="col-xs-12 col-md-6">
           <div class="progress">
-            <div class="progress-bar" role="progressbar" style="width: 90%;">
-            </div>
+            <div class="progress-bar" role="progressbar" style="width: 90%;"></div>
             <span class="progress-type">HTML / HTML5</span>
             <span class="progress-completed">90%</span>
           </div><!--/.progress -->
           <div class="progress">
-            <div class="progress-bar" role="progressbar" style="width: 80%;">
-            </div>
+            <div class="progress-bar" role="progressbar" style="width: 80%;"></div>
             <span class="progress-type">WORDPRESS</span>
             <span class="progress-completed">80%</span>
           </div><!--/.progress -->
           <div class="progress">
-            <div class="progress-bar" role="progressbar" style="width: 80%;">
-            </div>
+            <div class="progress-bar" role="progressbar" style="width: 80%;"></div>
             <span class="progress-type">WINDOWS XP, 7, 8, 10 &amp; SERVER 2008, 2012</span>
             <span class="progress-completed">80%</span>
           </div><!--/.progress -->
           <div class="progress">
-            <div class="progress-bar" role="progressbar" style="width: 75%;">
-            </div>
+            <div class="progress-bar" role="progressbar" style="width: 75%;"></div>
             <span class="progress-type">ABBYY FINEREADER OCR &amp; KURTWEIL 3000</span>
             <span class="progress-completed">75%</span>
           </div><!--/.progress -->
           <div class="progress">
-            <div class="progress-bar" role="progressbar" style="width: 70%;">
-            </div>
+            <div class="progress-bar" role="progressbar" style="width: 70%;"></div>
             <span class="progress-type">MOBILE DEVELOPMENT (ANDROID &amp; IOS)</span>
             <span class="progress-completed">70%</span>
           </div><!--/.progress -->
           <div class="progress">
-            <div class="progress-bar" role="progressbar" style="width: 70%;">
-            </div>
+            <div class="progress-bar" role="progressbar" style="width: 70%;"></div>
             <span class="progress-type">PHOTOSHOP</span>
             <span class="progress-completed">70%</span>
           </div><!--/.progress -->
         </div><!--/.col-xs-12 col-md-6 -->
         <div class="col-xs-12 col-md-6">
           <div class="progress">
-            <div class="progress-bar" role="progressbar" style="width: 60%;">
-            </div>
+            <div class="progress-bar" role="progressbar" style="width: 60%;"></div>
             <span class="progress-type">PHP / MYSQL</span>
             <span class="progress-completed">60%</span>
           </div><!--/.progress -->
           <div class="progress">
-            <div class="progress-bar" role="progressbar" style="width: 60%;">
-            </div>
+            <div class="progress-bar" role="progressbar" style="width: 60%;"></div>
             <span class="progress-type">LINUX</span>
             <span class="progress-completed">60%</span>
           </div><!--/.progress -->
           <div class="progress">
-            <div class="progress-bar" role="progressbar" style="width: 80%;">
-            </div>
+            <div class="progress-bar" role="progressbar" style="width: 60%;"></div>
             <span class="progress-type">GIT</span>
             <span class="progress-completed">60%</span>
           </div><!--/.progress -->
           <div class="progress">
-            <div class="progress-bar" role="progressbar" style="width: 60%;">
-            </div>
+            <div class="progress-bar" role="progressbar" style="width: 60%;"></div>
             <span class="progress-type">JS &amp; JQUERY</span>
             <span class="progress-completed">60%</span>
           </div><!--/.progress -->
           <div class="progress">
-            <div class="progress-bar" role="progressbar" style="width: 50%;">
-            </div>
+            <div class="progress-bar" role="progressbar" style="width: 50%;"></div>
             <span class="progress-type">RUBY (ON RAILS)</span>
             <span class="progress-completed">50%</span>
           </div><!--/.progress -->
         </div><!--/.col-xs-12 col-md-6 -->
       </div><!--/.row -->
-    </div><!--./container -->
+    </div> <!--./container -->
   </section>
 
   <section id="experience">
@@ -162,40 +165,65 @@
         <h2>Work Experience <small>My previous associations</small></h2>
       </div><!--/.page-header -->
       <div class="row">
-        <div class="col-xs-4 col-md-2 worktime mh">
-          <div class="years">Oct, 2010<br/>Present</div>
-        </div> <!--/.worktime -->
-        <div class="col-xs-8 col-md-10 details mh">
-          <h4>Systems- &amp; Network Engineer<br/>
-            <small><i class="fa fa-map-marker"> </i><a href="https://plus.google.com/105807787564855848545/about"> NoXqs B.V., Elst (gld)</a></small>
-          </h4>
-          <p>NoXqs makes apps, web applications and websites. The expertise lies in the easy accessibility of technology and information. The company now has about 25 employees. The mission of the company is "going for the possibilities." In other words,
-            go for the capabilities of the software users, including many people with intellectual disabilities. NoXqs also goes for the potential of employees; including many professionals with a form of autism.</p>
-        </div><!--/.details -->
-      </div><!--/.row -->
-      <div class="row">
-        <div class="col-xs-4 col-md-2 worktime mh">
-          <div class="years">2008<br/>Sept, 2010</div>
-        </div><!--/.worktime -->
-        <div class="col-xs-8 col-md-10 details mh">
-          <h4>ICT Coördinator Assistant<br/>
-            <small><i class="fa fa-map-marker"> </i><a href="http://prisma-arnhem.nl"> Het Prisma, Arnhem</a></small>
-          </h4>
-          <p>I assisted the ICT Coordinator with all sorts of tasks, and even lifted a whole project off the ground that's still going strong.</p>
-        </div><!--/.details -->
-      </div><!--/.row -->
-      <div class="row">
-        <div class="col-xs-4 col-md-2 worktime mh">
-          <div class="years">2008<br/>Sept, 2010</div>
-        </div><!--/.worktime -->
-        <div class="col-xs-8 col-md-10 details mh">
-          <h4>Employee Computer Advies Team<br/>
-            <small><i class="fa fa-map-marker"> </i><a href="http://www.deonderwijsspecialisten.nl"> De Onderwijsspecialisten, Arnhem </a></small>
-          </h4>
-          <p>ICT gives children and young people with disabilities the opportunity to belong, to express themselves and stay informed. By using the right IT means they are more independent in life and to increase their employment prospects.</p>
-          <p>The Education Specialists provide specific ICT support to pupils with physical, mental or multiple disabilities and chronically ill children.</p>
-          <p>My contribution was to follow the latest developments and trends. I was also responsible to provide what adjustments were lent etc...</p>
-        </div><!--/.details -->
+        <ul class="timeline">
+          <li>
+            <div class="timeline-years">
+              <span>Oct, 2010 - Present</span>
+            </div>
+            <div class="timeline-panel">
+              <div class="timeline-heading">
+                <h4>Systems- &amp; Network Engineer
+                <br/><small><i class="fa fa-map-marker"> </i> <a target="_blank" href="http://noxqs.nl">NoXqs B.V., Elst (gld)</a></small>
+                </h4>
+              </div>
+              <div class="timeline-body">
+                <p class="text-muted">
+                  NoXqs makes apps, web applications and websites. The expertise lies in the easy accessibility of technology and information. The company now has about 25 employees. The mission of the company is "going for the possibilities." In other words, go for the
+                  capabilities of the software users, including many people with intellectual disabilities. NoXqs also goes for the potential of employees; including many professionals with a form of autism.
+                </p>
+              </div>
+            </div>
+            <div class="line"></div>
+          </li>
+          <li>
+            <div class="timeline-years">
+              <span>2008 - Sept, 2010</span>
+            </div>
+            <div class="timeline-panel">
+              <div class="timeline-heading">
+                <h4>ICT Coördinator Assistant
+                <br/><small><i class="fa fa-map-marker"></i> <a target="_blank" href="http://prisma-arnhem.nl">Het Prisma, Arnhem</a></small>
+                </h4>
+              </div>
+              <div class="timeline-body">
+                <p class="text-muted">
+                  I assisted the ICT Coordinator with all sorts of tasks, and even lifted a whole project off the ground that's still going strong.
+                </p>
+              </div>
+            </div>
+            <div class="line"></div>
+          </li>
+          <li>
+            <div class="timeline-years">
+              <span>2008 - Sept, 2010</span>
+            </div>
+            <div class="timeline-panel">
+              <div class="timeline-heading">
+                <h4>Employee Computer Advies Team
+                <br/><small><i class="fa fa-map-marker"></i> <a target="_blank" href="http://www.deonderwijsspecialisten.nl">De Onderwijsspecialisten, Arnhem</a></small>
+                </h4>
+              </div>
+              <div class="timeline-body">
+                <p class="text-muted">
+                  ICT gives children and young people with disabilities opportunity's to belong, to express themselves &amp; stay informed.
+                  <br/>By using the right IT means they are more independent in life and to increase their employment prospects.
+                  <br/>De Onderwijsspecialisten provide specific ICT support to pupils with physical, mental or multiple disabilities and chronically ill children.
+                  <br/>My contribution was to follow the latest developments and trends. I was also responsible to provide what adjustments were lent etc...
+                </p>
+              </div><!--/.timeline-body -->
+            </div><!--/.timeline-panel -->
+          </li><!--/li -->
+        </ul><!--/.timeline -->
       </div><!--/.row -->
     </div><!--/.container -->
   </section>
@@ -206,41 +234,64 @@
         <h2>Education &amp; Diploma's <small>My (boring) education carreer </small></h2>
       </div><!--/.page-header -->
       <div class="row">
-        <div class="col-xs-8 col-md-10 details mh ">
-          <h4>Practical Sector Orientation (VMBO)<br/>
-            <small><i class="fa fa-map-marker"> </i> <a href="http://www.mariendael.nl/">Mariëndael, Arnhem</a></small>
-          </h4>
-          <p>Mariëndael provides secondary special education for students with physical disabilities and chronically ill students aged 12 to 20 years. The school guides students to be independently as possible and integrated into society.</p>
-        </div><!--/.details -->
-        <div class="col-xs-4 col-md-2 worktime mh">
-          <div class="years">2004<br/>2009</div>
-        </div><!--/.worktime -->
-        <div class="col-xs-8 col-md-10 details mh">
-          <h4>Basic Education<br/>
-            <small><i class="fa fa-map-marker"> </i> <a href="http://www.ssonml.nl/ulingshof/">Mytylschool Ulingshof, Venlo</a></small>
-          </h4>
-          <p>Mytylschool Ulingshof special school is a school where education and physiotherapy are integrated. The program is tailored to individual development opportunities.</p>
-        </div><!--/.details -->
-        <div class="col-xs-4 col-md-2 worktime mh">
-          <div class="years">2000<br/>2004</div>
-        </div><!-- /.worktime -->
+        <ul class="timeline">
+          <li>
+            <div class="timeline-years">
+              <span>2004 - 2009</span>
+            </div>
+            <div class="timeline-panel">
+              <div class="timeline-heading">
+                <h4>Practical Sector Orientation (VMBO)
+                <br/><small><i class="fa fa-map-marker"> </i> <a target="_blank" href="http://noxqs.nl">Mariëndael, Arnhem</a></small>
+                </h4>
+              </div>
+              <div class="timeline-body">
+                <p class="text-muted">
+                  Mariëndael provides secondary special education for students with physical disabilities and chronically ill students aged 12 to 20 years. The school guides students to be independently as possible and integrated into society.
+                </p>
+              </div>
+            </div>
+            <div class="line"></div>
+          </li>
+          <li>
+            <div class="timeline-years">
+              <span>2000 - 2004</span>
+            </div>
+            <div class="timeline-panel">
+              <div class="timeline-heading">
+                <h4>Basic Education
+                <br/><small><i class="fa fa-map-marker"></i> <a target="_blank" href="http://www.ssonml.nl/ulingshof/">Mytylschool Ulingshof, Venlo</a></small>
+                </h4>
+              </div>
+              <div class="timeline-body">
+                <p class="text-muted">
+                  Mytylschool Ulingshof special school is a school where education and physiotherapy are integrated. The program is tailored to individual development opportunities.
+                </p>
+              </div>
+            </div>
+          </li>
+        </ul><!--/.timeline -->
       </div><!--/.row -->
-    </div><!-- /.container -->
+    </div><!--/.container -->
   </section>
 
   <section id="portfolio">
     <div class="container">
       <div class="page-header">
-        <h2>Portfolio <small>Masterpieces I created</small></h2>
+        <h2>Portfolio
+          <small>Masterpieces I created</small>
+        </h2>
       </div>
       <!--/.page-header -->
       <div class="portfolio-filter">
-        <ul class=" nav nav-tabs">
-          <li class="active"><a href="#" data-filter="*">All</a></li>
-          <li><a href="#" data-filter=".website">Websites</a></li>
-          <li><a href="#" data-filter=".logo">Logo's</a></li>
-          <li><a href="#" data-filter=".other">Other</a></li>
-        </ul><!--/.nav-tabs -->
+        <div class="tabbable-line">
+          <ul class=" nav nav-tabs">
+            <li class="active"><a href="#" data-filter="*">All</a></li>
+            <li><a href="#" data-filter=".website">Websites</a></li>
+            <li><a href="#" data-filter=".logo">Logo's</a></li>
+            <li><a href="#" data-filter=".other">Other</a></li>
+          </ul><!--/.nav-tabs -->
+        </div><!--/.tabbable-line -->
       </div><!--/.portfolio-filter -->
 
       <div class="row isotope-container">
